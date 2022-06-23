4c4d162118213fe1d4d592b39ffb88017d2f5eb0
Alice Liu <aliceliu@jlsemi.com>
2022-06-23 18:19:23 -0400

    remove versions, adjust the click behaviors and display version on the top right bar

--- src/js/08-get-user-info.js
+++ src/js/08-get-user-info.js
@@ -1,27 +1,29 @@
-import 'cross-fetch/polyfill'
-;(function initUserProfile () {
+;(function () {
   'use strict'
-  var el = document.querySelector('#username')
-  const fetch = require('node-fetch')
-  fetch('/v1/api/user', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'x-csrf-token': '<%= csrf %>',
-      // 'Authorization': 'Bearer ' + token
-    },
-  }).then((response) => {
-    if (response.ok) {
-      return response.json()
-    } else {
-      console.log('err', response)
-    }
-  }).then((json) => {
-    el.textContent = 'Hi, ' + json.name + '!'
-    el.title = json.email
-  }).catch((err) => {
-    console.log(err)
-  })
-})()
+  var fetch = window.fetch
+  initUserProfile()
 
-// initUserProfile()
+  function initUserProfile () {
+    'use strict'
+    var el = document.querySelector('#username')
+    fetch && fetch('/v1/api/user', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'x-csrf-token': '<%= csrf %>',
+        // 'Authorization': 'Bearer ' + token
+      },
+    }).then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        console.log('err', response)
+      }
+    }).then((json) => {
+      el.textContent = 'Hi, ' + json.name + '!'
+      el.title = json.email
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+})()
